df2df7b67c5b6d91bc74de0570f9d556b08385e6
xRiKix <85706972+Riki744@users.noreply.github.com>
2022-07-10 10:22:56 +0300

    Update calc.py

--- 100_Days_of_Python/Day-10/calc.py
+++ 100_Days_of_Python/Day-10/calc.py
@@ -27,22 +27,25 @@ operator = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
-for key in operator:
-    print(key)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the first second?: "))
+def calculator():
+  num1 = float(input("What's the first number?: "))
+  for key in operator:
+      print(key)
+  should_continue = True
+  
+  while should_continue:
+    operation_symbol = input("Pick an operation from the line above: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operator[operation_symbol]
+    answer = calculation_function(num1, num2)
+  
+  
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+  
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
 
-calculation_function = operator[operation_symbol]
-first_answer = calculation_function(num1, num2)
-
-
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-
-operation_symbol = input("Pick another operation: ")
-num3 = int(input("What's the next number?: "))
-calculation_function = operator[operation_symbol]
-second_answer = calculation_function(calculation_function(num1, num2), num3)
-
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
-
+calculator()
