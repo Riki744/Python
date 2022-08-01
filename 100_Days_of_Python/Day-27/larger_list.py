623dbd623ab2406282f25b45934ed14a7cc0fb02
xRiKix <85706972+Riki744@users.noreply.github.com>
2022-08-01 20:59:47 +0300

    Create larger_list.py

---
+++ 100_Days_of_Python/Day-27/larger_list.py
@@ -0,0 +1,8 @@
+def larger_list(lst1, lst2):
+  if len(lst1) >= len(lst2):
+    return lst1[-1]
+  return lst2[-1]
+
+
+#Uncomment the line below when your function is done
+#print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10,]))
