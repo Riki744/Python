55b15b2bcf06c947c4e6fa7f65a9ff1b5cac075d
xRiKix <85706972+Riki744@users.noreply.github.com>
2022-08-01 21:00:21 +0300

    Create combine_sort.py

---
+++ 100_Days_of_Python/Day-27/combine_sort.py
@@ -0,0 +1,9 @@
+def combine_sort(lst1, lst2):
+  unsorted = lst1 + lst2
+  new_list = sorted(unsorted)
+  return unsorted
+  return new_list
+
+
+#Uncomment the line below when your function is done
+#print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
