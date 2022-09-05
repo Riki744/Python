84ba2e6e560fa9b78737de07c150cbf8252bfbbd
xRiKix <85706972+Riki744@users.noreply.github.com>
2022-09-05 21:14:06 +0300

    Create write_json.py

---
+++ 100_Days_of_Python/Day - 39/write_json.py
@@ -0,0 +1,14 @@
+import json
+
+data_payload = [
+  {'interesting message': 'What is JSON? A web application\'s little pile of secrets.',
+   'follow up': 'But enough talk!'}
+]
+
+with open("data.json", "w") as data_json:
+  json.dump(data_payload, data_json)
+
+#Check what we wrote to file
+with open("data.json") as file_json:
+  json_file = file_json.read()
+print(json_file)
