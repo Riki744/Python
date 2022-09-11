2b63ef5932b3ef056f72016b9c00c3e6393f15f6
xRiKix <85706972+Riki744@users.noreply.github.com>
2022-09-11 20:48:46 +0300

    Create Basta_Fazoolin.py

---
+++ 100_Days_of_Python/Day - 42/Basta_Fazoolin.py
@@ -0,0 +1,106 @@
+class Business:
+  def __init__(self, name, franchises):
+    self.name = name
+    self.franchises = franchises
+
+
+#Creating the Franchises
+class Franchise:
+  def __init__(self, address, menus):
+    self.address = address
+    self.menus = menus
+
+  def __repr__(self):
+    return self.address
+
+  def available_menus(self, time):
+    available_menu = []
+    for menu in self.menus:
+      if time >= menu.start_time and time <= menu.end_time:
+        available_menu.append(menu)
+    return available_menu
+
+
+class Menu:
+  #Constructor
+  def __init__(self, name, items, start_time, end_time):
+    self.name = name
+    self.items = items
+    self.start_time = start_time
+    self.end_time = end_time
+
+#Sttring representation
+  def __repr__(self):
+    return self.name + ' menu available from ' + str(self.start_time) + ' - ' + str(self.end_time)
+
+#Making a method for calculating bill
+  def calculate_bill(self, purchased_items):
+    bill = 0
+    for purchased_item in purchased_items:
+      if purchased_item in self.items:
+        bill += self.items[purchased_item]
+    return bill
+
+#Brunch Menu
+brunch_items = {
+  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+}
+
+brunch_menu = Menu("Brunch", brunch_items, 1100, 1600)
+
+#Early Bird Menu
+early_bird_items = {
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}
+
+early_bird_menu = Menu("Early Bird", early_bird_items, 1500, 1800)
+
+#Dinner Bird Menu
+dinner_items = {
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}
+
+dinner_menu = Menu("Dinner", dinner_items, 1700, 2300)
+
+
+#Kids Menu
+kids_items = {
+  'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+}
+
+kids_menu = Menu("Kids", dinner_items, 1100, 2100)
+
+#Creating menu array 
+menus = [brunch_menu, early_bird_menu, dinner_menu, kids_menu]
+
+flagship_store = Franchise('1232 West End Road', menus)
+new_installment = Franchise('12 East Mulberry Street', menus)
+
+
+basta = Business("Basta Fazoolin' with my Heart", [flagship_store, new_installment])
+
+#Arepa stuff
+arepas_items = {
+  'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50
+}
+
+arepas_menu = Menu("Take a Arepa", arepas_items, 1000, 2000)
+arepas_place = Franchise("189 Fitzgerald Avenue", [arepas_menu])
+arepa = Business("Take a Arepa", [arepas_place])
+
+
+#Testing string representation for Menu & Franchise
+#print(kids_menu)
+#print(flagship_store)
+#print(new_installment)
+
+#Testing calculate bill function
+#print(brunch_menu.calculate_bill(['pancakes', 'home fries', 'coffee']))
+#print(early_bird_menu.calculate_bill(['salumeria plate', 'mushroom ravioli (vegan)']))
+
+#Testing available_menu function
+#print(flagship_store.available_menus(1200))
+#print(new_installment.available_menus(1700))
+
+#Testing arepa
+#print(arepa.franchises[0])
