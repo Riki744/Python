b45e72fc024071954dc7d00ec0413a8469fd5fd7
xRiKix <85706972+Riki744@users.noreply.github.com>
2022-09-19 22:45:20 +0300

    Create add_exclamation.py

---
+++ 100_Days_of_Python/Day - 44/add_exclamation.py
@@ -0,0 +1,13 @@
+#Create a function named add_exclamation that has one parameter named word. This function should add exclamation points to the end of word until word is 20 characters long. If word is already at least 20 characters long, just return word.
+
+def add_exclamation(word):
+  lenght = 20
+  while (len(word) < 20):
+    word += "!"
+  return word
+
+# Uncomment these function calls to test your function:
+#print(add_exclamation("Codecademy"))
+# should print Codecademy!!!!!!!!!!
+#print(add_exclamation("Codecademy is the best place to learn"))
+# should print Codecademy is the best place to learn
